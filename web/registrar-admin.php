369ffc297d93337ebce4347121e2e443033bae18
Rubén Rodríguez Bayón <rubyman428@gmail.com>
2024-05-27 17:29:22 +0200

    Update registrar-admin.php

--- web/registrar-admin.php
+++ web/registrar-admin.php
@@ -12,7 +12,7 @@ $query = mysqli_query($con, $sql);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="CSS/style2.css" rel="stylesheet">
+    <link href="CSS/style.css" rel="stylesheet">
     <title>Inicio</title>
     <script src="javascript/script.js"></script>
     <link rel="icon" href="MEDIA/jibagod.webp">
